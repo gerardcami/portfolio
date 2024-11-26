064d9e8b4996ca575e3943126d04862222dba65f
Gerard Camí <gerardcamigay@gmail.com>
2024-11-26 23:55:37 +0100

    Structure defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import { BriefcaseBusiness, FolderCode, UserRound, Mail } from "lucide-react";
 import "./App.css";
 
 function App() {
@@ -8,29 +8,36 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-          Title
-        </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <BriefcaseBusiness />
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <FolderCode />
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <UserRound />
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <Mail />
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section>{/* Experience */}</section>
+        <section>{/* Projects */}</section>
+        <section>{/* About */}</section>
+        <section>{/* Contact */}</section>
+      </main>
     </>
   );
 }
