b877146624df9b8ea2d9d9fe62367d907dbe3d6e
Gerard Camí <gerardcamigay@gmail.com>
2024-11-28 10:19:16 +0100

    nav css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from "react";
-
 import { BriefcaseBusiness, Code, UserRound, Mail } from "lucide-react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <header>
-        <div>
-          <nav>
-            <ul>
-              <li>
+      <header className="sticky z-50 flex min-h-[72px] content-center overflow-x-auto">
+        <div className="mx-auto flex max-w-screen-xl items-center justify-between px-6 py-4 md:justify-around">
+          <nav className="hidden w-full justify-between md:flex">
+            <ul className="flex items-center gap-8 text-sm font-medium">
+              <li className="inline-flex items-center justify-center gap-x-2 text-sm font-medium">
                 <BriefcaseBusiness />
                 <a href="#experience">Experience</a>
               </li>
-              <li>
+              <li className="inline-flex items-center justify-center gap-x-2 text-sm font-medium">
                 <Code />
                 <a href="#projects">Projects</a>
               </li>
-              <li>
+              <li className="inline-flex items-center justify-center gap-x-2 text-sm font-medium">
                 <UserRound />
                 <a href="#about">About</a>
               </li>
-              <li>
+              <li className="inline-flex items-center justify-center gap-x-2 text-sm font-medium">
                 <Mail />
                 <a href="#contact">Contact</a>
               </li>
